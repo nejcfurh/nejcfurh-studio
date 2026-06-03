5b47ccccde751ea4c709c8338e433c352f5f6a6a
nejcfurh <nejc.furh7@gmail.com>
2026-06-03 15:30:49 +0200

    feat(nejcfurh-studio): Added a apps-template to apps

---
+++ apps/apps-template/utils/constants/page.data.ts
@@ -0,0 +1,10 @@
+export const PAGE_TITLE = 'Apps Template | A template for building apps.';
+
+export const PAGE_DESCRIPTION = 'A template for building apps.';
+
+export const OPENGRAPH_TITLE = PAGE_TITLE;
+export const OPENGRAPH_DESCRIPTION = PAGE_DESCRIPTION;
+
+export enum PageName {
+  APPS_TEMPLATE = 'Apps Template'
+}
